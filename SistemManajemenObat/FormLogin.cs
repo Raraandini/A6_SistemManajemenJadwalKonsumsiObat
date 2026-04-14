289d9d5345b4fe564cffcbd0e945a6d0633d0ca3
Raraandinii <raraandinib@gmail.com>
2026-04-15 04:33:24 +0700

    inisialisasi koneksi string dan efek hover tombol login

---
+++ SistemManajemenObat/FormLogin.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemManajemenObat
+{
+    public partial class FormLogin : Form
+    {
+        private readonly string connectionString =
+            "Data Source=DESKTOP-8U71E72\\RARA;Initial Catalog=DBSistemManajemenObat;Integrated Security=True";
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            btnLogin.MouseEnter += (s, ev) => btnLogin.BackColor = Color.FromArgb(111, 168, 220);
+            btnLogin.MouseLeave += (s, ev) => btnLogin.BackColor = Color.FromArgb(167, 199, 231);
+        }
+
+        
+
+       
+        
+    }
+}
