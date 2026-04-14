9c57e2ffc5aad26a1ad527b088b93f0f27f50f65
Raraandinii <raraandinib@gmail.com>
2026-04-15 04:41:02 +0700

    inisialisasi namespace dan string koneksi database jadwal

---
+++ SistemManajemenObat/FormJadwalKonsumsi.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemManajemenObat
+{
+    public partial class FormJadwalKonsumsi : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=DESKTOP-8U71E72\\RARA;Initial Catalog=DBSistemManajemenObat;Integrated Security=True";
+
+        public FormJadwalKonsumsi()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        
+    }
+}
